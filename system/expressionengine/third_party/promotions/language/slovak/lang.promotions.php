cb4185299655f913c2b159851729a1eeb1ac1d01
Mario Palka <mario.palka@krea.com>
2013-08-07 10:07:02 +0200

    Formatted lang files

--- system/expressionengine/third_party/promotions/language/slovak/lang.promotions.php
+++ system/expressionengine/third_party/promotions/language/slovak/lang.promotions.php
@@ -1,199 +1,158 @@
 <?php
 
 $lang = array(
-
-'promotions_module_name'				=> 'Súťaže',
-'promotions_module_description'			=> 'Modul súťazě pre ExpressionEngine',
-
+	'promotions_module_name' => 'Súťaže',
+	'promotions_module_description' => 'Modul súťazě pre ExpressionEngine',
 //global
 
-'return'								=> 'Spät',
-'edit'									=> 'Upraviť',
-'delete'								=> 'Odstrániť',
-'delete_confirm'						=> 'Naozaj chcete odstrániť požadovanú položku?',
-'report'								=> 'Odpovede',
-'setup'									=> 'Nastavenia',
-'enabled'								=> 'povoliť',
-'disabled'								=> 'zablokovať',
-'reset_filter'							=> 'Obnoviť',
-'and'									=> 'a',
-'cancel'								=> 'zrušiť',
-'yes'								=> 'Áno',
-'no'								=> 'Nie',
-'show_respond'						=> 'Podrobnosti',
-'ip_address'						=> 'IP adresa',
-
-'email'								=> 'Email',
-'accept_terms'						=> 'Súhlas s pravidlami',
-
-'error_module_settings_failed_title' => 'Chyba',
-'error_module_settings_failed_body' => 'Modul nie je nakonfigurovaný.<br />Pred použitím je potrebné modul správne nastaviť.',
-
-'error_message_header' => 'Odoslaný formulár obsahuje nasledovné chyby',
-'error_message_campaign_is_not_active'	=> 'Súťaž nie je aktívna.',
-
-'error_message_required_fields' => 'Nasledovné polia sú vyžadované',
-'error_message_invalid_fields' => 'Nasledovné polia sú vyplnené správne',
-'error_message_email_used'	=> 'Váš email nie je možné použiť v jednej súťaži viackrát.',
-'error_message_accept_terms'	=> 'Musíte súhlasiť s podmienkami.',
-'error_message_wrong_captcha'		=> 'Kontrolný text nebol správne opísaný.',
-
+	'return' => 'Spät',
+	'edit' => 'Upraviť',
+	'delete' => 'Odstrániť',
+	'delete_confirm' => 'Naozaj chcete odstrániť požadovanú položku?',
+	'report' => 'Odpovede',
+	'setup' => 'Nastavenia',
+	'enabled' => 'povoliť',
+	'disabled' => 'zablokovať',
+	'reset_filter' => 'Obnoviť',
+	'and' => 'a',
+	'cancel' => 'zrušiť',
+	'yes' => 'Áno',
+	'no' => 'Nie',
+	'show_respond' => 'Podrobnosti',
+	'ip_address' => 'IP adresa',
+	'email' => 'Email',
+	'accept_terms' => 'Súhlas s pravidlami',
+	'error_module_settings_failed_title' => 'Chyba',
+	'error_module_settings_failed_body' => 'Modul nie je nakonfigurovaný.<br />Pred použitím je potrebné modul správne nastaviť.',
+	'error_message_header' => 'Odoslaný formulár obsahuje nasledovné chyby',
+	'error_message_campaign_is_not_active' => 'Súťaž nie je aktívna.',
+	'error_message_required_fields' => 'Nasledovné polia sú vyžadované',
+	'error_message_invalid_fields' => 'Nasledovné polia sú vyplnené správne',
+	'error_message_email_used' => 'Váš email nie je možné použiť v jednej súťaži viackrát.',
+	'error_message_accept_terms' => 'Musíte súhlasiť s podmienkami.',
+	'error_message_wrong_captcha' => 'Kontrolný text nebol správne opísaný.',
 //mesages
 
-'message_operation_success'				=> 'Operácia prebehla úspešne',
-'message_reeorder_success'				=> 'Polia boli úspešne usporiadané',
-'message_campaigns_no_campaign_match_that_criteria'	=> 'Žiadna kampaň nevyhovuje požiadavkám na zobrazenie.',
-'message_no_responds_match_that_criteria' => 'Žiadna odpoveď nevyhovuje požiadavkám na zobrazenie.',
-'message_settings_save_success'			=> 'Nastavenia boli úspešne uložené.',
-
-'error_validate_marked_field_is_required'	=> 'Polia označené * sú povinné',
-'error_validate_wrong_format'				=> 'Niektoré polia neboli vyplnené správne',
-'error_validate_field_is_not_unique'		=> 'Uistite sa prosím, že hodnoty vo vyznačených poliach sú jedinečné',
-
+	'message_operation_success' => 'Operácia prebehla úspešne',
+	'message_reeorder_success' => 'Polia boli úspešne usporiadané',
+	'message_campaigns_no_campaign_match_that_criteria' => 'Žiadna kampaň nevyhovuje požiadavkám na zobrazenie.',
+	'message_no_responds_match_that_criteria' => 'Žiadna odpoveď nevyhovuje požiadavkám na zobrazenie.',
+	'message_settings_save_success' => 'Nastavenia boli úspešne uložené.',
+	'error_validate_marked_field_is_required' => 'Polia označené * sú povinné',
+	'error_validate_wrong_format' => 'Niektoré polia neboli vyplnené správne',
+	'error_validate_field_is_not_unique' => 'Uistite sa prosím, že hodnoty vo vyznačených poliach sú jedinečné',
 //special buttons
 
-'right_nav_button_settings' 	=> 'Nastavenia',
-
+	'right_nav_button_settings' => 'Nastavenia',
 //nav
 
-'button_back_to_campaigns' 						=> 'Späť na súťaže',
-'button_back_to_settings'						=> 'Späť na nastavenia',
-'button_back_to_custom_fields'					=> 'Späť na voliteľné polia',
-
+	'button_back_to_campaigns' => 'Späť na súťaže',
+	'button_back_to_settings' => 'Späť na nastavenia',
+	'button_back_to_custom_fields' => 'Späť na voliteľné polia',
 //titles
 
-'title_campaigns'					=> 'Súťaže',
-	'title_create_campaign'			=> 'Vytvoriť novú súťaž',
-'title_settings'				=> 'Nastavenia súťaží',
-	'title_settings_general'	=> 'Hlavné nastavenia',
-	'title_settings_custom_fields'	=> 'Voliteľné polia',
-		'title_settings_custom_fields_new' => 'Nové voliteľné pole',
-		'title_settings_custom_fields_edit' => 'Upraviť voliteľné pole',
-
+	'title_campaigns' => 'Súťaže',
+	'title_create_campaign' => 'Vytvoriť novú súťaž',
+	'title_settings' => 'Nastavenia súťaží',
+	'title_settings_general' => 'Hlavné nastavenia',
+	'title_settings_custom_fields' => 'Voliteľné polia',
+	'title_settings_custom_fields_new' => 'Nové voliteľné pole',
+	'title_settings_custom_fields_edit' => 'Upraviť voliteľné pole',
 //settings
 
-'settings_general_settings'	=> 'Hlavné nastavenia',
+	'settings_general_settings' => 'Hlavné nastavenia',
 	'settings_general_settings_desc' => 'Nastavte priečinok pre nahrávanie súborov a šablónu pre zobrazenie náhľadu.',
-	'settings_upload_settings_failed' => 'Priečinok pre nahrávanie súborov nie je nastavený',	
-'settings_custom_fields'	=> 'Vlastné polia',
+	'settings_upload_settings_failed' => 'Priečinok pre nahrávanie súborov nie je nastavený',
+	'settings_custom_fields' => 'Vlastné polia',
 	'settings_custom_fields_desc' => 'Vytvorte a spravuje voliteľné polia',
-
-
 //settings CF
 
-'settings_custom_fields_no_fields' 	=> 'Žiadne polia neboli vytvorené.',
-'settings_custom_fields_new' 		=> 'Vytvoriť nové voliteľné pole',
-
-'settings_custom_fields_label_field_type' => 'Typ poľa',
-'settings_custom_fields_label_field_label' => 'Názov poľa',
-'settings_custom_fields_label_field_label_desc' => 'Tento názov sa bude zobrazovať v administrácii počas vytvárania kampane.',
-'settings_custom_fields_label_field_name' => 'Skrátený názov poľa',
-'settings_custom_fields_label_field_name_desc' => 'Jedno slovo, bez medzier, podržítka a pomlčky sú povolené.',
-
-'settings_custom_fields_button_create_field'	=> 'Vytvoriť pole',
-'settings_custom_fields_button_update_field'	=> 'Upraviť pole',
-
+	'settings_custom_fields_no_fields' => 'Žiadne polia neboli vytvorené.',
+	'settings_custom_fields_new' => 'Vytvoriť nové voliteľné pole',
+	'settings_custom_fields_label_field_type' => 'Typ poľa',
+	'settings_custom_fields_label_field_label' => 'Názov poľa',
+	'settings_custom_fields_label_field_label_desc' => 'Tento názov sa bude zobrazovať v administrácii počas vytvárania kampane.',
+	'settings_custom_fields_label_field_name' => 'Skrátený názov poľa',
+	'settings_custom_fields_label_field_name_desc' => 'Jedno slovo, bez medzier, podržítka a pomlčky sú povolené.',
+	'settings_custom_fields_button_create_field' => 'Vytvoriť pole',
+	'settings_custom_fields_button_update_field' => 'Upraviť pole',
 //settings GE
-	
-"settings_general_settings_label_image_upload_dir"			=> 'Nastavenie nahrávania súborov',
-	"settings_general_settings_label_image_upload_dir_desc"	=> 'Zvoľte priečinok..',
-'settings_general_settings_label_live_look_template' => 'Šablóna pre náhľad',
+
+	"settings_general_settings_label_image_upload_dir" => 'Nastavenie nahrávania súborov',
+	"settings_general_settings_label_image_upload_dir_desc" => 'Zvoľte priečinok..',
+	'settings_general_settings_label_live_look_template' => 'Šablóna pre náhľad',
 	'settings_general_settings_label_live_look_template_desc' => 'Zvoľte šablónu',
-
-'settings_general_button_submit' => 'Uložiť zmeny',
-
-
-
+	'settings_general_button_submit' => 'Uložiť zmeny',
 //buttons
 
-'campaigns_button_new' 		=> 'Vytvoriť novú súťaž',
-'campaigns_button_search' 		=> 'Zobraziť súťaže',
-
+	'campaigns_button_new' => 'Vytvoriť novú súťaž',
+	'campaigns_button_search' => 'Zobraziť súťaže',
 //filter
 
-'campaigns_filter_per_page' => 'výsledkov na stranu',
-'campaigns_filter_sites_all'	=> 'Všetky stránky', 
-'campaigns_filter_keyword' => 'kľúčové slovo',
-
-'campaigns_filter_status_all' => 'Všetky súťaže',
-'campaigns_filter_status_scheduled' => 'Plánované súťaže',
-'campaigns_filter_status_active' => 'Aktívna súťaže',
-'campaigns_filter_status_paused' => 'Pozastavené súťaže',
-'campaigns_filter_status_ended' => 'Ukončené súťaže',
-'campaigns_filter_status_winners_announced' => 'Vyhodnotené súťaže',
-'campaigns_filter_status_draft' => 'Drafts',
-
+	'campaigns_filter_per_page' => 'výsledkov na stranu',
+	'campaigns_filter_sites_all' => 'Všetky stránky',
+	'campaigns_filter_keyword' => 'kľúčové slovo',
+	'campaigns_filter_status_all' => 'Všetky súťaže',
+	'campaigns_filter_status_scheduled' => 'Plánované súťaže',
+	'campaigns_filter_status_active' => 'Aktívna súťaže',
+	'campaigns_filter_status_paused' => 'Pozastavené súťaže',
+	'campaigns_filter_status_ended' => 'Ukončené súťaže',
+	'campaigns_filter_status_winners_announced' => 'Vyhodnotené súťaže',
+	'campaigns_filter_status_draft' => 'Drafts',
 	'campaigns_status_scheduled' => 'Scheduled',
 	'campaigns_status_active' => 'Active',
 	'campaigns_status_paused' => 'Paused',
 	'campaigns_status_ended' => 'Ended',
 	'campaigns_status_winners_announced' => 'Winners announced',
 	'campaigns_status_draft' => 'Návrhy',
-
-'campaigns_filter_date_all' => 'Všetky',
-'campaigns_filter_date_today' => 'Dnes',
-'campaigns_filter_date_yesterday' => 'Zajtra',
-'campaigns_filter_date_this_month' => 'Tento mesiac',
-'campaigns_filter_date_last_month' => 'Minulý mesiac',
-'campaigns_filter_date_this_year' => 'Tento rok',
-'campaigns_filter_date_last_year' => 'Minulý rok',
-
+	'campaigns_filter_date_all' => 'Všetky',
+	'campaigns_filter_date_today' => 'Dnes',
+	'campaigns_filter_date_yesterday' => 'Zajtra',
+	'campaigns_filter_date_this_month' => 'Tento mesiac',
+	'campaigns_filter_date_last_month' => 'Minulý mesiac',
+	'campaigns_filter_date_this_year' => 'Tento rok',
+	'campaigns_filter_date_last_year' => 'Minulý rok',
 //campaign
 
-'title_campaign_overview' => 'Krok 1: Základné informácie',
-
-'campaign_label_campaign_type' 			=> "Typ súťaže",
-'campaign_label_campaign_title' 		=> "Názov súťaže",
-'campaign_label_campaign_url_title' 	=> "URL názov súťaže",
-
-'campaign_label_start_date' 	=> "Začiatok súťaže",
-'campaign_label_end_date' 		=> "Koniec súťaže",
-
-'campaing_overview_button_continue' => 'Pokračovať',
-
-'title_campaign_content' => 'Krok 2: Obsah',
-'campaign_label_image'	=> 'Obrázok<br /><small>Odporúčané rozmery 830x240px</small>',
-'campaign_label_head_title' => 'Nadpis<br /><small>Zadajte nadpis na stránke so súťažou.</small>',
-'campaign_label_head_note' => 'Text v hlavičke<br /><small>Predstavenie súťaže, príp. produktu s ktorým súťaž súvisí.</small>',
-'campaign_label_foot_note' => 'Text v pätičke<br /><small>Doplnkové alebo kontaktné informácie.</small>',
-'campaign_label_terms' => 'Pravidlá a podmienky<br /><small>Zobrazia sa v popup okne, pokiaľ je súhlas s podmienkami vyžadovaný.</small>',
-
-'campaing_content_button_back' => 'Späť',
-'campaing_content_button_continue' => 'Pokračovať',
-
-'title_campaign_completition' => 'Step 4: Vyhodnotenie',
-'campaign_label_return_url' => 'Návratová URL adresa<br /><small>Zadate, pokiaľ chcete užívateľa presmerovať na konkrétnu URL adresu namiesto ďakovnej stránky.</small>',
-'campaing_completition_button_continue' => 'Dokončiť',
-'campaign_label_custom_fields' => 'Voliteľné polia',
+	'title_campaign_overview' => 'Krok 1: Základné informácie',
+	'campaign_label_campaign_type' => "Typ súťaže",
+	'campaign_label_campaign_title' => "Názov súťaže",
+	'campaign_label_campaign_url_title' => "URL názov súťaže",
+	'campaign_label_start_date' => "Začiatok súťaže",
+	'campaign_label_end_date' => "Koniec súťaže",
+	'campaing_overview_button_continue' => 'Pokračovať',
+	'title_campaign_content' => 'Krok 2: Obsah',
+	'campaign_label_image' => 'Obrázok<br /><small>Odporúčané rozmery 830x240px</small>',
+	'campaign_label_head_title' => 'Nadpis<br /><small>Zadajte nadpis na stránke so súťažou.</small>',
+	'campaign_label_head_note' => 'Text v hlavičke<br /><small>Predstavenie súťaže, príp. produktu s ktorým súťaž súvisí.</small>',
+	'campaign_label_foot_note' => 'Text v pätičke<br /><small>Doplnkové alebo kontaktné informácie.</small>',
+	'campaign_label_terms' => 'Pravidlá a podmienky<br /><small>Zobrazia sa v popup okne, pokiaľ je súhlas s podmienkami vyžadovaný.</small>',
+	'campaing_content_button_back' => 'Späť',
+	'campaing_content_button_continue' => 'Pokračovať',
+	'title_campaign_completition' => 'Step 4: Vyhodnotenie',
+	'campaign_label_return_url' => 'Návratová URL adresa<br /><small>Zadate, pokiaľ chcete užívateľa presmerovať na konkrétnu URL adresu namiesto ďakovnej stránky.</small>',
+	'campaing_completition_button_continue' => 'Dokončiť',
+	'campaign_label_custom_fields' => 'Voliteľné polia',
 	'campaign_label_custom_fields_empty' => 'Žiadne voliteľné polia nie sú vytvorené',
-	
-'campaign_label_mailing_lists' => 'Odber správ',
+	'campaign_label_mailing_lists' => 'Odber správ',
 	'campaign_label_mailing_lists_empty' => 'Žiadne zoznamy pre odber správ nie sú definované',
-	
-'campaign_label_use_email' => 'Vyžadovať od užívateľa email?',
+	'campaign_label_use_email' => 'Vyžadovať od užívateľa email?',
 	'field_required' => "Áno, vyplnenie emailu bude povinné",
 	'field_not_required' => "Nie, pole s emailom nebude zobrazené",
 	'field_optional' => "Nie, vyplnenie emailu nebude povinné, ale pole s emailom bude zobrazené",
-	'field_unique' => "Áno, vyplnenie emailu bude povinné a email nesmie byť v rovnakej súťaži použitý viackrát",		
-'campaign_label_use_terms_of_service' => 'Vyžadovať súhlas s podmienkami súťaźe',
-'campaign_label_use_captcha' => 'Vyžadovať obrázkový kód Captcha?',
-
-'campaign_label_options' => 'Možnosti',
-'campaign_label_paused' => 'Kampaň je pozastavená<br /><small>Kampaň sa nebude zobrazovať, dokedy tento príznak nebude zrušený.</small>',
-'campaign_label_winners_announced' => 'Oznámenie výhercov<br /><small>Napíšte textový oznam, ktorý bude publikovaný na stránke v sekcii "Výhercovia súťaže".</small>',
-
-
-'campaigns_label_mass_action'	=> 'Vykonať...',
-'campaigns_label_mass_action_status' => 'Príznak pozastavená súťaž',
-'campaigns_label_mass_action_paused_on'	=> 'Nastaviť',
-'campaigns_label_mass_action_paused_off' => 'Zrušiť',
-
-'campaigns_button_commit'	=> 'Vykonať',
-'button_back_to_report' => 'Späť na odpovede',
-
-'custom_field_required' => "Povinné",
-'custom_field_not_required' => "Nepovinné",
-
-
-"" => ""
+	'field_unique' => "Áno, vyplnenie emailu bude povinné a email nesmie byť v rovnakej súťaži použitý viackrát",
+	'campaign_label_use_terms_of_service' => 'Vyžadovať súhlas s podmienkami súťaźe',
+	'campaign_label_use_captcha' => 'Vyžadovať obrázkový kód Captcha?',
+	'campaign_label_options' => 'Možnosti',
+	'campaign_label_paused' => 'Kampaň je pozastavená<br /><small>Kampaň sa nebude zobrazovať, dokedy tento príznak nebude zrušený.</small>',
+	'campaign_label_winners_announced' => 'Oznámenie výhercov<br /><small>Napíšte textový oznam, ktorý bude publikovaný na stránke v sekcii "Výhercovia súťaže".</small>',
+	'campaigns_label_mass_action' => 'Vykonať...',
+	'campaigns_label_mass_action_status' => 'Príznak pozastavená súťaž',
+	'campaigns_label_mass_action_paused_on' => 'Nastaviť',
+	'campaigns_label_mass_action_paused_off' => 'Zrušiť',
+	'campaigns_button_commit' => 'Vykonať',
+	'button_back_to_report' => 'Späť na odpovede',
+	'custom_field_required' => "Povinné",
+	'custom_field_not_required' => "Nepovinné",
+	"" => ""
 );
